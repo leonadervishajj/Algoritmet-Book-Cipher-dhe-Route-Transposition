65f72e1b473cdf04d18ba6b9b0d3deccd32d9bae
leonadervishajj <leona.dervishaj@student.uni-pr.edu>
2026-04-10 20:13:53 +0200

    Pjesa e ndarjes dhe pastrimit te tekstit

--- book_cipher.py
+++ book_cipher.py
@@ -1,4 +1,18 @@
+def normalize_text(text):
+    return text.lower().strip()
+
+def split_text(text):
+    return text.split()  #dy funksione që e kthejnë tekstin në lowercase dhe i heq hapësirat e panevojshme. dhe funksioni tjetër e ndan tekstin në fjalë
+
+
+
 def create_index(book_text):
     words = book_text.lower().split
     index = {}
     
+    for i, word in enumerate(words):
+        if word not in index:
+            index[word] = []
+        index[word].append(i)
+        
+    return index      #krijon nje dictionary ku çdo fjale e tekstit reference lidhet me listen e pozicioneve te saj ne liber
